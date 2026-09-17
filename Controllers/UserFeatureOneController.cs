1a31454422f697b7920c1b43cb72b0fea7ecda02
Shubham Hupta <shubham.gupta19701@gmail.com>
2026-09-17 16:34:15 +0530

    UserFeature1

---
+++ Controllers/UserFeatureOneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCwithDocker.Controllers
+{
+    public class UserFeatureOneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
